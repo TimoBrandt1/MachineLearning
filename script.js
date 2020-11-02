7ae1cb1691fb474423fc179b9f87b4a5e85d4841
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2020-11-02 15:23:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-//Count to 5
-//1-5, 5-1
-
 const trainingData = [
   [1,2,3,4,5],
   [5,4,3,2,1],
   [5,6,7,8,9,10]
+  [6,8,10,12,14,16]
 ];
 
 const net = new brain.recurrent.LSTMTimeStep();
@@ -14,3 +12,4 @@ net.train(trainingData);
 console.log(net.run([1,2,3,4]));
 console.log(net.run([5,4,3,2]));
 console.log(net.run([5,6,7,8,9]));
+console.log(net.run([6,8,10,12,14]));
